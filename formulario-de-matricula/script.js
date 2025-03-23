dc357c2b2114f3dfcd960180b37a7a74b2d524cc
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-03-23 12:25:35 -0300

    corrigindo o mobile-mode

--- formulario-de-matricula/script.js
+++ formulario-de-matricula/script.js
@@ -3,28 +3,38 @@ let mobileMain = document.querySelector('main')
 let mobileAside = document.querySelector('aside')
 let backBtn = document.querySelector('#back-btn')
 
+// Verifica o tamanho da tela ao carregar e ao redimensionar
+window.addEventListener('resize', checkScreen)
+checkScreen()
+
 formBtn.addEventListener('click', clicar)
 backBtn.addEventListener('click', voltar)
 
-if (window.innerWidth >= 1280) {
-    mobileMain.style.display = 'block'
-    mobileAside.style.display = 'block'
+function checkScreen() {
+    if (window.innerWidth >= 1024) {
+         // Mostra tudo no desktop
+        mobileMain.classList.remove ('mobile-hidden')
+        mobileAside.classList.remove ('mobile-hidden')
+    } else {
+        // Mostra apenas o aside no mobile
+        mobileMain.classList.add('mobile-hidden')
+        mobileAside.classList.remove('mobile-hidden')
+    }
 }
 
 function clicar() {
-    if (window.innerWidth < 1280) {
-        mobileMain.style.display = 'block'
-        mobileAside.style.display = 'none'
+    // No mobile, alterna entre main e aside
+    if (window.innerWidth < 1024) {
+        mobileMain.classList.remove('mobile-hidden')
+        mobileAside.classList.add('mobile-hidden')    
     }
 }
 
 function voltar() {
-    if (window.innerWidth < 1280) {
-        mobileMain.style.display = 'none'
-        mobileAside.style.display = 'block'
-    } else {
-        mobileMain.style.display = 'block'
-        mobileAside.style.display = 'block'
+    // No mobile, volta pro aside
+    if (window.innerWidth < 1024) {
+        mobileMain.classList.add('mobile-hidden')
+        mobileAside.classList.remove('mobile-hidden')
     }
 }
 

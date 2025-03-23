9afa2c3d1d809eb246b687ac8711918c77761a56
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-03-23 10:22:48 -0300

    Update script.js

--- formulario-de-matricula/script.js
+++ formulario-de-matricula/script.js
@@ -6,6 +6,11 @@ let backBtn = document.querySelector('#back-btn')
 formBtn.addEventListener('click', clicar)
 backBtn.addEventListener('click', voltar)
 
+if (window.innerWidth >= 1280) {
+    mobileMain.style.display = 'block'
+    mobileAside.style.display = 'block'
+}
+
 function clicar() {
     if (window.innerWidth < 1280) {
         mobileMain.style.display = 'block'
@@ -17,10 +22,9 @@ function voltar() {
     if (window.innerWidth < 1280) {
         mobileMain.style.display = 'none'
         mobileAside.style.display = 'block'
+    } else {
+        mobileMain.style.display = 'block'
+        mobileAside.style.display = 'block'
     }
 }
 
-if (window.innerWidth >= 1280) {
-    mobileMain.style.display = 'block'
-    mobileAside.style.display = 'block'
-}

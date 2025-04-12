e023159df552d49e150ee75800002c781c73e88d
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-04-11 21:51:04 -0300

    criando a parte de customização de cores, adicionando temas.

---
+++ formulario-de-convite/script.js
@@ -0,0 +1,10 @@
+const radios = document.querySelectorAll('.radio-box input[type="radio"]');
+
+radios.forEach(radio => {
+  radio.addEventListener('change', () => {
+    document.querySelectorAll('.radio-box').forEach(box => {
+      box.classList.remove('selected');
+    });
+    radio.parentElement.classList.add('selected');
+  });
+});

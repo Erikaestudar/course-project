5434869d5849d2893cd450f7894e5c450c4a1a12
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-04-16 15:40:49 -0300

    droparea e switch

--- formulario-de-convite/script.js
+++ formulario-de-convite/script.js
@@ -1,4 +1,6 @@
 let radios = document.querySelectorAll('.radio-box input[type="radio"]');
+let checkbox = document.getElementById('switch');
+let label = document.querySelector('span #mode-label');
 
 radios.forEach(radio => {
   radio.addEventListener('change', () => {
@@ -6,5 +8,15 @@ radios.forEach(radio => {
       box.classList.remove('selected');
     });
     radio.parentElement.classList.add('selected');
+  });
+});
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const checkbox = document.getElementById('switch');
+  const label = document.getElementById('mode-label');
+
+  checkbox.addEventListener('change', () => {
+    label.textContent = checkbox.checked ? 'Claro' : 'Escuro';
   });
 });

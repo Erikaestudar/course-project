3b67ed3b0df849595391161679a279007cb08ed0
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-04-16 21:57:15 -0300

    criando seção dos contatos, seção dos checkbox com os termos e privacidade e o botão de enviar

--- formulario-de-convite/script.js
+++ formulario-de-convite/script.js
@@ -1,6 +1,36 @@
+let form = document.querySelector('form')
+let inputs = form.querySelectorAll('input[required]')
+
+form.addEventListener('submit', (e) => {
+  let isValid = true;
+
+  inputs.forEach(input => {
+      let errorDiv = input.nextElementSibling; // pega a div .error
+      if (!input.value.trim()) {
+          input.classList.add('invalid'); 
+          errorDiv.style.display = 'flex';
+          isValid = false;
+      } else {
+          errorDiv.style.display = 'none';
+          input.classList.remove('invalid');
+      }
+  });
+
+  if (!isValid) {
+      e.preventDefault(); // impede o envio do form se tiver erro
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  let checkbox = document.getElementById('switch');
+  let label = document.getElementById('mode-label');
+
+  checkbox.addEventListener('change', () => {
+    label.textContent = checkbox.checked ? 'Claro' : 'Escuro';
+  });
+});
+
 let radios = document.querySelectorAll('.radio-box input[type="radio"]');
-let checkbox = document.getElementById('switch');
-let label = document.querySelector('span #mode-label');
 
 radios.forEach(radio => {
   radio.addEventListener('change', () => {
@@ -11,12 +41,3 @@ radios.forEach(radio => {
   });
 });
 
-
-window.addEventListener('DOMContentLoaded', () => {
-  const checkbox = document.getElementById('switch');
-  const label = document.getElementById('mode-label');
-
-  checkbox.addEventListener('change', () => {
-    label.textContent = checkbox.checked ? 'Claro' : 'Escuro';
-  });
-});

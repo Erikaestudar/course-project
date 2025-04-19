d1e92d3296028e71202c61c6f70c7e7e51205d20
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-04-19 16:28:11 -0300

    Update script.js

--- formulario-de-convite/script.js
+++ formulario-de-convite/script.js
@@ -30,14 +30,4 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-let radios = document.querySelectorAll('.radio-box input[type="radio"]');
-
-radios.forEach(radio => {
-  radio.addEventListener('change', () => {
-    document.querySelectorAll('.radio-box').forEach(box => {
-      box.classList.remove('selected');
-    });
-    radio.parentElement.classList.add('selected');
-  });
-});
 
